78531630063aaa7fe14df5082719b5188bb1aa2a
Christian <157312367+christiandosreisdev@users.noreply.github.com>
2024-02-11 18:00:12 -0300

    second commit

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const fullList = document.querySelector('.list-task')
 let myItemsList = []
 
 function addNewTask() {
-    myItemsList.push(input.value)
+    myItemsList.push({
+        task: input.value,
+        complete: false
+    })
 
     input.value = ''
     
@@ -16,21 +19,42 @@ function showTask() {
 
     let newLi = ''
 
-    myItemsList.forEach((task, index) => {
+    myItemsList.forEach((item, index) => {
         newLi = newLi + `
-        <li class="task">
-        <img src="./img/checked.png" alt="check-na-tarefa">
-        <p>${task}</p>
+        <li class="task ${item.complete && 'done'}">
+        <img src="./img/checked.png" alt="check-na-tarefa" onclick="completeTask(${index})">
+        <p class="text">${item.task}</p>
         <img src="./img/trash.png" alt="tarefa-exluída" onclick="deletItem(${index})">
         </li>       
       `
     })
 
     fullList.innerHTML = newLi
+
+    localStorage.setItem('task', JSON.stringify(myItemsList))
+}
+
+function completeTask (index) {
+   myItemsList[index].complete = !myItemsList[index].complete
+
+   showTask()
 }
 
 function deletItem(index) {
+    myItemsList.splice(index, 1)
     console.log(index)
+
+    showTask()
 }
 
+function reloadTask() {
+    const localStorageTasks = localStorage.getItem('list')
+    if(localStorageTasks){
+        myItemsList = JSON.parse(localStorageTasks)
+    }
+
+    showTask()
+}
+
+reloadTask()
 button.addEventListener('click', addNewTask)
